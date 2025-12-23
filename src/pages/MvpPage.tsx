db2b981429d9b314a774075c6f1be9640883493c
Lucas Azevedo Costa <eozecos@gmail.com>
2025-12-23 12:07:30 -0300

    feat(pages): criando a pagina demonstrativa do mvp

---
+++ src/pages/MvpPage.tsx
@@ -0,0 +1,5 @@
+import { Mvp } from "../components/mvp/Mvp";
+
+export function MvpPage() {
+  return <Mvp />;
+}
--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -9,8 +9,14 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as MvpRouteImport } from './routes/mvp'
 import { Route as IndexRouteImport } from './routes/index'
 
+const MvpRoute = MvpRouteImport.update({
+  id: '/mvp',
+  path: '/mvp',
+  getParentRoute: () => rootRouteImport,
+} as any)
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
@@ -19,28 +25,39 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/mvp': typeof MvpRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/mvp': typeof MvpRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/mvp': typeof MvpRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/mvp'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/mvp'
+  id: '__root__' | '/' | '/mvp'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  MvpRoute: typeof MvpRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
+    '/mvp': {
+      id: '/mvp'
+      path: '/mvp'
+      fullPath: '/mvp'
+      preLoaderRoute: typeof MvpRouteImport
+      parentRoute: typeof rootRouteImport
+    }
     '/': {
       id: '/'
       path: '/'
@@ -53,6 +70,7 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  MvpRoute: MvpRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
