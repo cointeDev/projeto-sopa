3d75ed66aec1e09e503587c755393f7970070ee3
LucasAzevedoCosta <eozecos@gmail.com>
2026-04-10 15:28:51 -0300

    fix: ajustando o modal de delegação de etapa

--- src/components/modals/ModalDelegacaoEtapa.tsx
+++ src/components/modals/ModalDelegacaoEtapa.tsx
@@ -77,7 +77,7 @@ export default function ModalDelegacaoEtapa({
 
 	return (
 		<Transition appear show={open} as={Fragment}>
-			<Dialog as="div" className="relative z-50" onClose={handleClose}>
+			<Dialog as="div" className="relative z-200" onClose={handleClose}>
 				<Transition.Child
 					as={Fragment}
 					enter="ease-out duration-300"
